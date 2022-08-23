873c01592345c1240a34239845ee3f343517f344
AnaClara81 <anaceceiza81@gmail.com>
2022-08-23 14:50:05 -0300

    agregando html y js

---
+++ js/index.js
@@ -0,0 +1,24 @@
+
+  
+/* for(let i =1; i < 10; i++ ) {
+
+  let i = parseInt(prompt("Ingrese numero"));
+  let numero = i;
+
+    if(numero %2 == 0){
+   alert(`${numero} Es numero par`);
+    break;
+  }else{
+    alert(`${numero} Es numero impar`);
+    break;
+  }
+  }*/
+
+  let nombre = prompt("Ingrese un nombre")
+   for (let i = 0; i < 10; i++) {
+    
+   nombre === i;
+   alert(`Hola ${nombre}`);
+   break;
+    
+  }
